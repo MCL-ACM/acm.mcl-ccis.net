c7351d0a42776e7091647d710f51c9186a3d28b4
JOB LIPAT <lipatjj@live.mcl.edu.ph>
2021-10-18 09:20:47 +0800

    added gradient to contact us

--- components/Home/ContactUs.vue
+++ components/Home/ContactUs.vue
@@ -1,7 +1,25 @@
 <template>
-  <div class="flex flex-row ml-auto">
-    <div>
-      <p class="font-bold text-3xl">Contact Us</p>
+  <div class="flex flex-row ml-auto h-40 flex flex-row">
+    <div class="ml-auto mr-14 my-auto">
+      <a
+        href="/contact"
+        class="font-bold text-4xl text-white flex flex-row items-center"
+      >
+        <p class="mr-2">Contact Us</p>
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          class="h-8 w-auto"
+          viewBox="0 0 20 20"
+          fill="currentColor"
+        >
+          <path
+            fill-rule="evenodd"
+            d="M10.293 3.293a1 1 0 011.414 0l6 6a1 1 0 010 1.414l-6 6a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-4.293-4.293a1 1 0 010-1.414z"
+            clip-rule="evenodd"
+          />
+        </svg>
+ 
+      </a>
     </div>
   </div>
 </template>
--- pages/index.vue
+++ pages/index.vue
@@ -3,16 +3,16 @@
     <div class="lg:px-32 bg-gradient-to-b from-sea-green to-transparent pb-10 pt-20">
       <HomeHeroSection />
     </div>
-    <div class="lg:px-32 pb-10">
+    <div class="lg:px-32 p-10">
       <HomeAboutUs />
     </div>
-    <div class="lg:px-32 pb-10">
+    <div class="lg:px-32 p-10">
       <HomeEvents />
     </div>
-    <div class="lg:px-32 mb-10">
+    <div class="lg:px-32 p-20">
       <HomeMembers />
     </div>
-    <div class="lg:px-32 mb-10">
+    <div class="lg:px-32 p-10 contact-us">
       <HomeContactUs />
     </div>
   </main>
@@ -24,6 +24,10 @@ export default {};
 
 <style lang="scss" scoped>
 .hero-section{
-   background-image: linear-gradient(to bottom right, rgba(44, 193, 199, 1), rgba(217, 237, 146, 0));
+   background-image: linear-gradient(to bottom, rgba(44, 193, 199, 1), rgba(217, 237, 146, 0));
+}
+
+.contact-us{
+   background-image: linear-gradient(to right, rgba(31, 97, 153, 1), rgba(139, 210, 189, 1));
 }
 </style>
