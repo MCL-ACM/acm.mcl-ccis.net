44c8ecfbcfceecd55794cf59f4717b2b14019baa
karlopenguini <karlobpalisoc@gmail.com>
2021-11-19 19:27:49 +0800

    modal responsive

--- components/Contact/Modal.vue
+++ components/Contact/Modal.vue
@@ -7,17 +7,17 @@
     flex
     justify-center
     items-center
-    
+    z-40
   ">
-    <div class="bg-white flex flex-col space-y-8 p-20 max-w-2xl rounded-3xl ">
-      <header class="text-5xl font-bold mb-4 text-dark-blue">
+    <div class=" bg-white flex flex-col space-y-8 md:p-20 p-10 m-16 max-w-2xl rounded-3xl ">
+      <header class="text-xl md:text-5xl font-bold md:mb-4 text-dark-blue">
         <slot name="header">
           Thank you for contacting us!
         </slot>
         
       </header>
 
-      <section class="text-2xl">
+      <section class="md:text-2xl">
         <slot name="body">
           There will be no official transactions using this service. For formal transactions and possible partnerships, we will reply with the email you have provided.
           If you have not received a reply from us, please make sure that you have provided the correct email address.
