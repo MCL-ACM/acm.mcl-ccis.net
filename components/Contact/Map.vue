06bc1b8c46301e76ff360f16cc503ae4bdc9b996
karlopenguini <karlobpalisoc@gmail.com>
2021-11-20 21:57:25 +0800

    contact us v.1.0

--- components/Contact/Map.vue
+++ components/Contact/Map.vue
@@ -1,10 +1,11 @@
 <template>
     <section>
         <h1 class="text-2xl font-bold mb-6 text-black">Address</h1>
+        
         <div class="relative h-full">
 
             <iframe  width="100%" height="240px" style="border:0" loading="lazy" allowfullscreen
-            src="https://www.google.com/maps/embed/v1/place?q=place_id:ChIJDXb8QZJivTMRdj05NWeEhJQ&key=AIzaSyB3c4EfGxfF3whBDIoigJeLM8sGqDMTVPQ"></iframe>
+            :src="mapENV"></iframe>
         
         </div>
         
@@ -14,8 +15,15 @@
 
 <script>
 export default {
+    data(){
+        return{
+            mapENV:process.env.MAP_KEY,
+        }
+        
+    },
 
 }
+
 </script>
 
 <style>
