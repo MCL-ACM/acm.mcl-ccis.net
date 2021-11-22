18d6b13d89ec4e8527c78e9bccab2ba693f03a86
karlopenguini <karlobpalisoc@gmail.com>
2021-11-22 18:42:55 +0800

    cursorPointer on hover event card top pdding remvd

--- components/Contact/Social.vue
+++ components/Contact/Social.vue
@@ -3,13 +3,13 @@
       <h1 class="text-2xl font-bold mb-6 text-black">Social Media</h1>
         <div class="flex flex-row w-full sm:flex-col sm:space-y-6 ">
             <div v-for="(item, index) in socialItems" :key="index" class="justify-center sm:justify-start flex flex-grow flex-row items-center space-x-4">
-                <div class="bg-blue h-14 w-14 p-4 rounded-full">
+                <div class="bg-blue h-14 w-14 p-4 rounded-full transition duration-75 transform hover:scale-105 hover:shadow-lg">
                     <a :href="item.link">
                         <img :src="item.icon" :alt="item.name" class="h-full w-auto" />
                     </a>
                     
                 </div>
-                <a class="font-extralight tracking-widest space-x-4 sm:flex hidden" :href="item.link">{{item.link}}</a>
+                <p class="font-extralight tracking-widest space-x-4 sm:flex hidden">{{item.link}}</p>
             </div>
         </div>
   </section>
