22392f8c5fd0f4baa8bf04f2f45bbbc6b6d7b982
charmaine <charmaine.rabano@gmail.com>
2021-11-23 11:42:52 +0800

    additional fixes on event modal

--- components/Events/Slideshow.vue
+++ components/Events/Slideshow.vue
@@ -1,11 +1,11 @@
 <template>
-  <div class="relative max-h-80 ">
+  <div class="relative h-40 md:h-60 lg:h-80 ">
       <slot></slot>
 
-      <button @click="prev" class="absolute p-2 top-1/2 bg-white left-1 rounded-full">
+      <button @click="prev" class="absolute p-2 top-1/2 bg-white left-1 rounded-full -mt-4">
         <img src="@/assets/svg/back.svg" alt="" class="h-5">
       </button>
-      <button @click="next" class="absolute p-2 top-1/2 bg-white right-1 rounded-full">
+      <button @click="next" class="absolute p-2 top-1/2 bg-white right-1 rounded-full -mt-4">
         <img src="@/assets/svg/next.svg" alt="" class="h-5 text-right">
       </button>
   </div>
