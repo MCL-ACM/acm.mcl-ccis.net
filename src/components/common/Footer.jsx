93bd9162165a89e55d7c7fd3679d5db9d44852d6
Karlo Miguel Palisoc <karlobpalisoc@gmail.com>
2022-03-13 17:12:58 +0800

    removed z index in footer to make icons clickable

--- src/components/common/Footer.jsx
+++ src/components/common/Footer.jsx
@@ -23,7 +23,7 @@ export default function Footer() { // TODO: Can someone try to set the height of
 	]
 
 	return (
-		<footer className="absolute bottom-0 w-full h-[34.8125em] z-[-100] ">
+		<footer className="absolute bottom-0 w-full h-[34.8125em] ">
 			<svg className="absolute" viewBox="0 0 375 48" preserveAspectRatio="none">
 				<path d="M0 0H375V47.7998C375 47.7998 317 0.510638 296 0.510638C275 0.510638 0 48 0 48V0Z" fill="#FFFFFF"/>
 			</svg>
@@ -35,14 +35,14 @@ export default function Footer() { // TODO: Can someone try to set the height of
 							<p className="text-lg">Pulo Diezmo Rd, Pulo, Cabuyao, Laguna 4026, Philippines</p>
 							<p className="text-lg">acm.mclstudchapter@gmail.com</p>
 						</article>
-						<article className="flex flex-col gap-y-[1.0625rem]">
+						<div className="flex flex-col gap-y-[1.0625rem]">
 							<header className="text-sm font-light">Keep in touch</header>
-							<figure className="flex gap-x-3.5">
+							<ul className="flex gap-x-3.5">
 								{
-									socialLinks.map(({link, icon}) => <a href={link}>{icon}</a>)
+									socialLinks.map(({link, icon}) => <a href={link} target="_blank">{icon}</a>)
 								}
-							</figure>
-						</article>
+							</ul>
+						</div>
 					</div>
 					<p className="text-lg font-light text-center">© {year} Malayan Colleges Laguna ACM Student Chapter</p>
 				</section>
