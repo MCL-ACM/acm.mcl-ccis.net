cedf41a9756f434c2a9e237e8366428d1a6ffdf1
Karlo Miguel Palisoc <karlobpalisoc@gmail.com>
2022-03-13 17:16:06 +0800

    fixed z index

--- src/components/common/Navbar.jsx
+++ src/components/common/Navbar.jsx
@@ -6,7 +6,7 @@ import Logo from "./navbar/Logo";
 
 export default function Navbar() {
 	return (
-		<nav className="fixed flex justify-between px-10 py-7 w-full bg-white items-center">
+		<nav className="fixed flex justify-between px-10 py-7 w-full bg-white items-center z-10">
 			<Link to="/">
 				<Logo/>
 			</Link>
--- src/components/common/navbar/MenuSidebar.jsx
+++ src/components/common/navbar/MenuSidebar.jsx
@@ -23,7 +23,7 @@ const sidebar = {
 
 export default function MenuSidebar() {
   return (
-    <motion.nav variants={sidebar} className="fixed pt-20 top-0 h-3/4 right-0 overflow-x-hidden w-[12rem] bg-white border-[1px] rounded-tl-2xl rounded-bl-2xl border-[#D1DCE4]">
+    <motion.nav variants={sidebar} className="z-50 fixed pt-20 top-0 h-3/4 right-0 overflow-x-hidden w-[12rem] bg-white border-[1px] rounded-tl-2xl rounded-bl-2xl border-[#D1DCE4]">
         <MenuLinks />
     </motion.nav>
   )
