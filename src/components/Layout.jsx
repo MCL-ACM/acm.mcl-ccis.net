f138d7c8b2486bc0ee3ce1a71b0611c9205bca35
Karlo Miguel Palisoc <karlobpalisoc@gmail.com>
2022-03-14 11:49:10 +0800

    Added button, components page, addtnl colors

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -1,5 +1,4 @@
 import React from "react";
-import Button from "./common/buttons/Button";
 import Footer from "./common/Footer";
 import Navbar from "./common/Navbar";
 
@@ -7,8 +6,7 @@ export default function Layout({ children }) {
   return (
     <main className="relative min-h-screen">
       <Navbar />
-      <section className="flex-auto pt-24 pb-[34.8125em]">
-        <Button text="My Button" color="oxford-blue"/>
+      <section className="pt-24 pb-[34.8125em]">
         {children}
       </section>
       <Footer />
--- src/components/common/buttons/Button.jsx
+++ src/components/common/buttons/Button.jsx
@@ -1,10 +1,20 @@
 import React from 'react'
 import { motion } from 'framer-motion'
-export default function Button({color, text, icon, callback}) {
+export default function Button({glow, textColor ,color, text, icon, callback}) {
+
+  let isGlowing = glow ? "shadow-button-glow-white" : "shadow-lg"
+
   return (
-    <motion.button>
-        <i>{icon}</i>
-        <p>{text}</p>
+    <motion.button 
+      whileHover={{
+        y:'-6%'
+      }}
+      whileTap ={{
+        y:'0%'
+      }}
+      className={ isGlowing + " " + textColor + " " + color + " " + "py-[6px] px-6 flex flex-row justify-center align-middle items-center rounded-full"}> 
+        {icon}
+        <p className='pl-3 text-lg'>{text}</p>
         
     </motion.button>
   )
