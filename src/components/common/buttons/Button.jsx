49604d05ca45426eb0155159c66a2077556a4164
Karlo Miguel Palisoc <karlobpalisoc@gmail.com>
2022-03-15 13:28:30 +0800

    added button to event-component branch

---
+++ src/components/common/buttons/Button.jsx
@@ -0,0 +1,35 @@
+import React from "react"
+import { motion } from "framer-motion"
+export default function Button({
+	glow,
+	textColor,
+	color,
+	text,
+	icon,
+	callback,
+}) {
+	let isGlowing = glow ? "shadow-button-glow-white" : "shadow-lg"
+
+	return (
+		<motion.button
+			onClick={callback}
+			whileHover={{
+				y: "-6%",
+			}}
+			whileTap={{
+				y: "0%",
+			}}
+			className={
+				isGlowing +
+				" " +
+				textColor +
+				" " +
+				color +
+				" " +
+				"py-[6px] px-6 flex flex-row justify-center align-middle items-center rounded-full"
+			}>
+			{icon}
+			<p className='pl-3 text-lg'>{text}</p>
+		</motion.button>
+	)
+}
