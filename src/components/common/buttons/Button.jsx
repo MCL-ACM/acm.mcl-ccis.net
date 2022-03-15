34d9992e21f5c7dfa88b5fec7a756aeb3c2ccdf9
Karlo Miguel Palisoc <karlobpalisoc@gmail.com>
2022-03-15 13:56:18 +0800

    finished event card componenet

--- src/components/common/buttons/Button.jsx
+++ src/components/common/buttons/Button.jsx
@@ -1,35 +1,32 @@
-import React from "react"
-import { motion } from "framer-motion"
-export default function Button({
-	glow,
-	textColor,
-	color,
-	text,
-	icon,
-	callback,
-}) {
-	let isGlowing = glow ? "shadow-button-glow-white" : "shadow-lg"
+import React from 'react';
+import { motion } from 'framer-motion';
 
-	return (
-		<motion.button
-			onClick={callback}
-			whileHover={{
-				y: "-6%",
-			}}
-			whileTap={{
-				y: "0%",
-			}}
-			className={
-				isGlowing +
-				" " +
-				textColor +
-				" " +
-				color +
-				" " +
-				"py-[6px] px-6 flex flex-row justify-center align-middle items-center rounded-full"
-			}>
-			{icon}
-			<p className='pl-3 text-lg'>{text}</p>
-		</motion.button>
-	)
+export default function Button({
+  glow,
+  textColor,
+  color,
+  text,
+  icon,
+  callback,
+}) {
+  const isGlowing = glow ? 'shadow-button-glow-white' : 'shadow-lg';
+
+  return (
+    <motion.button
+      onClick={callback}
+      whileHover={{
+        y: '-6%',
+      }}
+      whileTap={{
+        y: '0%',
+      }}
+      className={
+        `${isGlowing} ${textColor} ${color} ` +
+        `py-[6px] px-6 flex flex-row justify-center align-middle items-center rounded-full`
+      }
+    >
+      {icon}
+      <p className='pl-3 text-lg'>{text}</p>
+    </motion.button>
+  );
 }
