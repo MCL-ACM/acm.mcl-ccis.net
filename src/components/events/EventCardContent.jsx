29644b64f5e3aa25a82ea494b0129f11b2c4f89b
JOB LIPAT <lipatjj@live.mcl.edu.ph>
2022-03-15 20:09:43 +0800

    small refactor on EventCardComponent

--- src/components/events/EventCardContent.jsx
+++ src/components/events/EventCardContent.jsx
@@ -11,20 +11,19 @@ export default function EventCardContent({
 }) {
   return (
     <div className='absolute flex flex-col items-center w-full gap-6 align-middle px-9'>
-      <ul
-        className={`${
-          tagged ? 'visible ' : 'hidden '
-        }flex space-x-3 absolute pt-7 px-9 w-full flex-wrap`}
-      >
-        {tags.map((tag) => (
-          <li
-            key={tag}
-            className='bg-standard-blue text-white font-bold text-xs py-[4px] px-[10px] rounded-lg'
-          >
-            {tag.toUpperCase()}
-          </li>
-        ))}
-      </ul>
+      {tagged && (
+        <ul className='absolute flex flex-wrap w-full space-x-3 pt-7 px-9'>
+          {tags.map((tag) => (
+            <li
+              key={tag}
+              className='bg-standard-blue text-white font-bold text-xs py-[4px] px-[10px] rounded-lg'
+            >
+              {tag.toUpperCase()}
+            </li>
+          ))}
+        </ul>
+      )}
+
       <header className='flex flex-col gap-2 text-center pt-[72px]'>
         <h1 className='text-lg font-bold text-oxford-blue'>{title}</h1>
         <h5 className='text-base font-light text-maximum-blue-green'>{year}</h5>
