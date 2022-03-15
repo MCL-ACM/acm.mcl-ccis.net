04edea6b58abfaf6552f5c4ca036652a443c908c
JOB LIPAT <lipatjj@live.mcl.edu.ph>
2022-03-15 20:02:26 +0800

    separated EventCard into different components

---
+++ src/components/events/CarouselEventCard.jsx
@@ -0,0 +1,83 @@
+import React, { useState } from 'react';
+import { motion, AnimatePresence } from 'framer-motion';
+import { wrap } from 'popmotion';
+import EventCard from './EventCard';
+import CarouselIndicator from './CarouselIndicator';
+import EventCardContent from './EventCardContent';
+
+const variants = {
+  enter: (direction) => ({
+    x: direction > 0 ? 1000 : -1000,
+    opacity: 0,
+  }),
+  center: {
+    zIndex: 1,
+    x: 0,
+    opacity: 1,
+  },
+  exit: (direction) => ({
+    zIndex: 0,
+    x: direction < 0 ? 1000 : -1000,
+    opacity: 0,
+  }),
+};
+
+const swipeConfidenceThreshold = 10000;
+const swipePower = (offset, velocity) => Math.abs(offset) * velocity;
+
+export default function CarouselEventCard({ events, tagged }) {
+  const [[page, direction], setPage] = useState([0, 0]);
+  const eventIndex = wrap(0, events.length, page);
+  const currentEvent = events[eventIndex];
+
+  const paginate = (newDirection) => {
+    setPage([page + newDirection, newDirection]);
+  };
+
+  return (
+    <EventCard>
+      <AnimatePresence initial={false} custom={direction}>
+        <motion.div
+          key={page}
+          custom={direction}
+          variants={variants}
+          initial='enter'
+          animate='center'
+          exit='exit'
+          transition={{
+            x: {
+              type: 'spring',
+              stiffness: 300,
+              damping: 30,
+            },
+            opacity: { duration: 0.2 },
+          }}
+          drag='x'
+          dragConstraints={{ left: 0, right: 0 }}
+          dragElastic={1}
+          onDragEnd={(e, { offset, velocity }) => {
+            const swipe = swipePower(offset.x, velocity.x);
+
+            if (swipe < -swipeConfidenceThreshold) {
+              paginate(1);
+            } else if (swipe > swipeConfidenceThreshold) {
+              paginate(-1);
+            }
+          }}
+          className='absolute flex flex-col items-center w-full h-full gap-6 align-middle px-9'
+        >
+          <EventCardContent
+            tagged={tagged}
+            tags={currentEvent.tags}
+            title={currentEvent.title}
+            year={currentEvent.title}
+            img={currentEvent.img}
+            imageAlt={currentEvent.imageAlt}
+            description={currentEvent.description}
+          />
+        </motion.div>
+      </AnimatePresence>
+      <CarouselIndicator eventCount={events.length} eventIndex={eventIndex} />
+    </EventCard>
+  );
+}
---
+++ src/components/events/EventCardContent.jsx
@@ -0,0 +1,38 @@
+import React from 'react';
+
+export default function EventCardContent({
+  tagged,
+  tags,
+  title,
+  year,
+  img,
+  imageAlt,
+  description,
+}) {
+  return (
+    <div className='absolute flex flex-col items-center w-full gap-6 align-middle px-9'>
+      <ul
+        className={`${
+          tagged ? 'visible ' : 'hidden '
+        }flex space-x-3 absolute pt-7 px-9 w-full flex-wrap`}
+      >
+        {tags.map((tag) => (
+          <li
+            key={tag}
+            className='bg-standard-blue text-white font-bold text-xs py-[4px] px-[10px] rounded-lg'
+          >
+            {tag.toUpperCase()}
+          </li>
+        ))}
+      </ul>
+      <header className='flex flex-col gap-2 text-center pt-[72px]'>
+        <h1 className='text-lg font-bold text-oxford-blue'>{title}</h1>
+        <h5 className='text-base font-light text-maximum-blue-green'>{year}</h5>
+      </header>
+      <img height='181px' width='263px' src={img} alt={imageAlt} />
+      <p className='text-sm font-light text-center text-rich-black'>
+        {description}
+      </p>
+    </div>
+  );
+}
