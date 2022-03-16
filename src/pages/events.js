9d138261298efeab93141ae42e0029571806537a
JOB LIPAT <lipatjj@live.mcl.edu.ph>
2022-03-16 22:35:08 +0800

    small refactor on events page

--- src/pages/events.js
+++ src/pages/events.js
@@ -2,7 +2,6 @@ import React, { useState } from 'react';
 import { graphql } from 'gatsby';
 import EventDropdown from '../components/events/EventDropdown';
 import EventCard from '../components/events/EventCard';
-import sampleImage from '../images/about/how_we_started.png';
 
 export default function events({ data }) {
   const [year, setYear] = useState('All');
@@ -21,16 +20,14 @@ export default function events({ data }) {
     return undefined;
   });
 
-  console.log(selectedEvents[0]);
-
   return (
     <div className='w-full'>
-      <div className='flex flex-col text-center items-center w-full'>
-        <header className='text-center pb-8 w-full'>
-          <h1 className='text-oxford-blue text-4xl font-bold'>
+      <div className='flex flex-col items-center w-full text-center'>
+        <header className='w-full pb-8 text-center'>
+          <h1 className='text-4xl font-bold text-oxford-blue'>
             Our Initiatives
           </h1>
-          <h3 className='text-darkish-blue text-2xl font-medium'>Events</h3>
+          <h3 className='text-2xl font-medium text-darkish-blue'>Events</h3>
         </header>
         <EventDropdown year={year} changeYear={setYear} />
         <EventCard shadow tagged carousel events={selectedEvents} />
