fcd4a86a7a44c02f9000032a6688d5f4bd6018c7
JOB LIPAT <lipatjj@live.mcl.edu.ph>
2022-03-16 21:28:53 +0800

    removed commented query

--- src/pages/members.js
+++ src/pages/members.js
@@ -8,46 +8,6 @@ export default function members({ data }) {
   const executiveCommittee = data.executive.member;
   const committees = data.committees.edges.map(({ node }) => node);
 
-  /*
-  const executiveCommittee = [
-    { photo: '', name: 'Khristian Kikuchi', position: 'Adviser' },
-    { photo: '', name: 'James Michael Paz', position: 'President' },
-    { photo: '', name: 'Karlo Miguel Palisoc', position: 'Vice President' },
-    { photo: '', name: 'Jayvee Mapote', position: 'Secretary' },
-    { photo: '', name: 'Patrick Mediodia', position: 'Treasurer' },
-  ];
-
-  const committees = [
-    {
-      name: 'Research and Development Committee',
-      chair: {
-        photo: '',
-        name: 'Job Lipat',
-        position: 'RND Committee Chair',
-      },
-      memberNames: [
-        'First Name Last Name',
-        'First Name Last Name',
-        'First Name Last Name',
-        'First Name Last Name',
-      ],
-    },
-    {
-      name: 'Membership Committee',
-      chair: {
-        photo: '',
-        name: 'John Arthur Panti',
-        position: 'Membership Committee Chair',
-      },
-      memberNames: [
-        'First Name Last Name',
-        'First Name Last Name',
-        'First Name Last Name',
-        'First Name Last Name',
-      ],
-    },
-  ];
-  */
   return (
     <div className='flex flex-col gap-20 px-5 mb-24'>
       <MembersHeader />
