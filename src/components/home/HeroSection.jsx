705ff131fb06a059a78abe0f1a53aecb21109916
JOB LIPAT <lipatjj@live.mcl.edu.ph>
2022-03-17 00:35:02 +0800

    move hero section to common components

--- src/components/home/HeroSection.jsx
+++ src/components/home/HeroSection.jsx
@@ -1,11 +1,20 @@
 import * as React from 'react';
-import HeroBanner from './HeroBanner';
+import { StaticImage } from 'gatsby-plugin-image';
+import HeroBanner from '../common/hero/HeroBanner';
 import HeroContent from './HeroContent';
 
 export default function HeroSection({ callback }) {
   return (
     <section>
-      <HeroBanner />
+      <HeroBanner
+        image={
+          <StaticImage
+            src='../../images/home/Home-Banner.jpg'
+            alt='Home Banner'
+          />
+        }
+      />
+
       <HeroContent callback={callback} />
     </section>
   );
