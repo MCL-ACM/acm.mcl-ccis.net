2f97ec9862f73d4b4d2327ecd91728c4809dfa17
JOB LIPAT <lipatjj@live.mcl.edu.ph>
2022-03-17 00:43:41 +0800

    moved divider to common components - also deleted obsolete components

--- src/components/events/EventCard.jsx
+++ src/components/events/EventCard.jsx
@@ -1,9 +1,10 @@
 import React from 'react';
+import Divider from '../common/Divider';
 
 export default function EventCard({ children }) {
   return (
     <article className='items-center w-[20.9375em] h-[34.25em] relative rounded-bl-3xl rounded-br-3xl border-[0.55px] border-gray-200 overflow-hidden bg-white'>
-      <span className='absolute block w-full h-[4px] bg-gradient-to-r from-cerulean-crayola to-standard-blue' />
+      <Divider className='h-[4px]' />
       {children}
     </article>
   );
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,7 +1,7 @@
 import React, { useRef } from 'react';
+import Divider from '../components/common/Divider';
 import AboutSection from '../components/home/AboutSection';
 import ContactSection from '../components/home/ContactSection';
-import Divider from '../components/home/Divider';
 import EventsSection from '../components/home/EventsSection';
 import HeroSection from '../components/home/HeroSection';
 import MembersSection from '../components/home/MembersSection';
@@ -14,11 +14,11 @@ export default function index() {
   return (
     <main className='flex flex-col gap-12'>
       <HeroSection callback={() => executeScroll()} />
-      <Divider glow />
+      <Divider glow className='mx-5' />
       <div ref={aboutRef}>
         <AboutSection />
       </div>
-      <Divider glow />
+      <Divider glow className='mx-5' />
       <MembersSection />
       <EventsSection />
       <ContactSection />
