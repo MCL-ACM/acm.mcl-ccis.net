dc195aa01c05e28e2c444d5e3f6f8019223a5930
Karlo Miguel Palisoc <karlobpalisoc@gmail.com>
2022-03-17 11:33:38 +0800

    centered captcha and button, fixed width of button

--- src/pages/contact-us.js
+++ src/pages/contact-us.js
@@ -1,12 +1,12 @@
-import React from 'react'
-import HeadingText from '../components/contact/HeadingText'
-import Form from '../components/contact/Form'
+import React from 'react';
+import HeadingText from '../components/contact/HeadingText';
+import Form from '../components/contact/Form';
 
 export default function contactUs() {
   return (
-    <div className="flex flex-col">
+    <div className='flex flex-col'>
       <HeadingText />
       <Form />
     </div>
-  )
+  );
 }
