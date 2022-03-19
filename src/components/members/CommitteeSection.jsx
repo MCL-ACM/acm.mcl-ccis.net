8fa496191b168e66b900d6ce259ceb33e1cec95f
JOB LIPAT <lipatjj@live.mcl.edu.ph>
2022-03-19 10:18:10 +0800

    changed layout of committee section

--- src/components/members/CommitteeSection.jsx
+++ src/components/members/CommitteeSection.jsx
@@ -6,16 +6,20 @@ import tempPhoto from '../../images/members/dog.jpg';
 export default function CommitteeSection({ name, chair, members }) {
   return (
     <MemberSection header={name}>
-      <div className='grid grid-cols-2 sm:grid-cols-3 gap-y-8  sm:gap-y-14'>
+      <div className='flex flex-row flex-wrap justify-center gap-y-8 sm:gap-y-14'>
         <MemberInfo
           name={chair.name}
           position={chair.position}
           photo={tempPhoto}
-          className='col-span-2 sm:col-span-3'
+          className='w-full'
         />
 
         {members.map((memberName) => (
-          <MemberInfo name={memberName} position='Member' />
+          <MemberInfo
+            name={memberName}
+            position='Member'
+            className='w-[50%] sm:w-[33%]'
+          />
         ))}
       </div>
     </MemberSection>
--- src/components/members/MemberSection.jsx
+++ src/components/members/MemberSection.jsx
@@ -3,12 +3,12 @@ import React from 'react';
 export default function MemberSection({ header, children }) {
   return (
     <div>
-      <hr className='sm:hidden h-1 shadow-lg shadow-blue-500/90 bg-gradient-to-tr from-standard-blue to-cerulean-crayola' />
-      <div className='mt-4 mb-8 sm:mb-14'>
-        <h3 className='sm:mb-8 text-2xl font-bold text-center text-oxford-blue sm:text-5xl sm:text-left'>
+      <hr className='h-1 shadow-lg sm:hidden shadow-blue-500/90 bg-gradient-to-tr from-standard-blue to-cerulean-crayola' />
+      <div className='mt-4 mb-8 sm:mb-16'>
+        <h3 className='text-2xl font-bold text-center sm:mb-8 text-oxford-blue sm:text-5xl sm:text-left'>
           {header}
         </h3>
-        <hr className='hidden sm:block h-1 shadow-lg shadow-blue-500/90 bg-gradient-to-tr from-standard-blue to-cerulean-crayola' />
+        <hr className='hidden h-1 shadow-lg sm:block shadow-blue-500/90 bg-gradient-to-tr from-standard-blue to-cerulean-crayola' />
       </div>
 
       {children}
