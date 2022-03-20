8ee07fbd4914f6a507377442a2d5d19606d2767a
JOB LIPAT <lipatjj@live.mcl.edu.ph>
2022-03-20 14:32:13 +0800

    fixed hero banner responsiveness

--- src/components/common/hero/HeroBanner.jsx
+++ src/components/common/hero/HeroBanner.jsx
@@ -5,13 +5,12 @@ import HeroBannerDecoration from './HeroBannerDecoration';
 
 export default function HeroBanner({ image }) {
   return (
-    <div className='relative w-full lg:mt-16'>
-      <div className=''>{image}</div>
-
-      <DotsRowSmallDecoration className='hidden sm:block sm:absolute -top-8 -left-10 max-w-2.5 max-h-2.5' />
-      <SquareDotsDecoration className='text-cerulean-crayola hidden sm:block sm:absolute -top-10 -right-10 max-w-72 max-h-72 z-[-1]' />
-      <SquareDotsDecoration className='text-cerulean-crayola hidden sm:block sm:absolute -bottom-10 -left-10 max-w-72 max-h-72 z-[-1]' />
-      <HeroBannerDecoration className='absolute lg:hidden w-[101%] -bottom-[2px]' />
+    <div className='relative lg:m-10'>
+      {image}
+      <DotsRowSmallDecoration className='hidden lg:block lg:absolute -top-8 -left-10 max-w-2.5 max-h-2.5' />
+      <SquareDotsDecoration className='text-cerulean-crayola hidden lg:block lg:absolute -top-10 -right-10 max-w-[60%] max-h-[60%] z-[-1]' />
+      <SquareDotsDecoration className='text-cerulean-crayola hidden lg:block lg:absolute -bottom-10 -left-10 max-w-[60%] max-h-[60%] z-[-1]' />
+      <HeroBannerDecoration className='absolute lg:hidden w-[100%] -bottom-[2px]' />
     </div>
   );
 }
