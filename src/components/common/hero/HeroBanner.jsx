bb6dd079f993f7d3a1b31f01c0de6c91796e3a88
JOB LIPAT <lipatjj@live.mcl.edu.ph>
2022-03-20 14:35:26 +0800

    adjusted hero banner

--- src/components/common/hero/HeroBanner.jsx
+++ src/components/common/hero/HeroBanner.jsx
@@ -5,9 +5,9 @@ import HeroBannerDecoration from './HeroBannerDecoration';
 
 export default function HeroBanner({ image }) {
   return (
-    <div className='relative lg:m-10'>
+    <div className='relative lg:m-10 lg:mt-14'>
       {image}
-      <DotsRowSmallDecoration className='hidden lg:block lg:absolute -top-8 -left-10 max-w-2.5 max-h-2.5' />
+      <DotsRowSmallDecoration className='hidden lg:block lg:absolute -top-14 -left-10 max-w-2 max-h-2' />
       <SquareDotsDecoration className='text-cerulean-crayola hidden lg:block lg:absolute -top-10 -right-10 max-w-[60%] max-h-[60%] z-[-1]' />
       <SquareDotsDecoration className='text-cerulean-crayola hidden lg:block lg:absolute -bottom-10 -left-10 max-w-[60%] max-h-[60%] z-[-1]' />
       <HeroBannerDecoration className='absolute lg:hidden w-[100%] -bottom-[2px]' />
--- src/components/common/hero/HeroSection.jsx
+++ src/components/common/hero/HeroSection.jsx
@@ -17,7 +17,7 @@ export default function HeroSection({
   };
 
   return (
-    <div className='flex flex-col lg:gap-16 fixed-width'>
+    <div className='flex flex-col lg:gap-16 fixed-width lg:mt-16'>
       <HeroBanner image={image} />
       <HeroHeadingText
         header={headerText}
