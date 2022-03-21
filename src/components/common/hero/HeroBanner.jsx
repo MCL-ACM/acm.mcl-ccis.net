4d5455e5c97fed8686f6607507c05e96fb49d822
charmaine <charmaine.rabano@gmail.com>
2022-03-21 18:00:49 +0800

    fixed incorrect 'resolve conflict'

--- src/components/common/hero/HeroBanner.jsx
+++ src/components/common/hero/HeroBanner.jsx
@@ -3,11 +3,14 @@ import DotsRowSmallDecoration from '../decorations/DotsRowSmallDecoration';
 import SquareDotsDecoration from '../decorations/SquareDotsDecoration';
 import HeroBannerDecoration from './HeroBannerDecoration';
 
-export default function HeroBanner({ image, className }) {
+export default function HeroBanner({ image }) {
   return (
-    <div className={`relative w-full h-auto ${className}`}>
+    <div className='relative lg:m-10 lg:mt-14'>
       {image}
-      <HeroBannerDecoration className='lg:hidden absolute w-[101%] -bottom-[2px]' />
+      <DotsRowSmallDecoration className='hidden lg:block lg:absolute -top-14 -left-10 max-w-2 max-h-2' />
+      <SquareDotsDecoration className='text-cerulean-crayola hidden lg:block lg:absolute -top-10 -right-10 max-w-[60%] max-h-[60%] z-[-1]' />
+      <SquareDotsDecoration className='text-cerulean-crayola hidden lg:block lg:absolute -bottom-10 -left-10 max-w-[60%] max-h-[60%] z-[-1]' />
+      <HeroBannerDecoration className='absolute lg:hidden w-[100%] -bottom-[2px]' />
     </div>
   );
 }
