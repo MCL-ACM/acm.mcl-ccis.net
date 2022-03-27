d26a0d93f227f9ba2dc6e1d80c53aace46b0104e
JOB LIPAT <lipatjj@live.mcl.edu.ph>
2022-03-27 18:40:43 +0800

    added members content: - added missing members - added pictures of executive committee - added pictures of committee chairs

--- src/components/members/ExecutiveSection.jsx
+++ src/components/members/ExecutiveSection.jsx
@@ -1,19 +1,18 @@
 import React from 'react';
 import MemberInfo from './MemberInfo';
 import MemberSection from './MemberSection';
-import tempPhoto from '../../images/members/dog.jpg';
 
 export default function ExecutiveSection({ officers }) {
   return (
     <MemberSection header='Executive Committee'>
       <div className='grid grid-cols-2 sm:grid-cols-4 gap-y-8 lg:gap-y-14'>
-        {officers.map(({ name, position }, index) => (
+        {officers.map(({ name, position, photo }, index) => (
           <div
             className={`flex flex-col items-center ${
               index === 0 && 'col-span-4'
             }`}
           >
-            <MemberInfo name={name} position={position} photo={tempPhoto} />
+            <MemberInfo name={name} position={position} photo={photo} />
           </div>
         ))}
       </div>
--- src/components/members/MemberInfo.jsx
+++ src/components/members/MemberInfo.jsx
@@ -1,11 +1,13 @@
 import React from 'react';
+import { GatsbyImage, getImage } from 'gatsby-plugin-image';
 
 export default function MemberInfo({ name, position, photo, alt, className }) {
+  const image = getImage(photo);
   return (
     <div className={`flex flex-col items-center text-center ${className}`}>
       {photo && (
-        <img
-          src={photo}
+        <GatsbyImage
+          image={image}
           alt={alt}
           className='w-24 h-24 mb-3 rounded-full lg:w-36 lg:h-36'
         />
