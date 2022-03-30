b0ec09d51000e0286391ff64eaaf373eba09604b
Karlo Miguel Palisoc <karlobpalisoc@gmail.com>
2022-03-30 11:30:04 +0800

    event modal fix

--- src/components/events/EventCardBackground.jsx
+++ src/components/events/EventCardBackground.jsx
@@ -1,14 +1,28 @@
 import React from 'react';
+import { motion } from 'framer-motion';
+
+const variants = {
+  rest: {
+    boxShadow: '8px 8px 0 2px hsla(199, 77%, 94%)',
+  },
+  hover: {
+    boxShadow: '15px 15px 0 2px hsla(199, 77%, 96%)',
+    originX: 25,
+  },
+  lineHover: {},
+};
 
 export default function EventCardBackground({ shadow, children, className }) {
-  const hasShadow = shadow ? 'shadow-xl' : '';
-
+  const hasShadow = shadow ? variants : {};
   return (
-    <article
-      className={`w-[20.9375em] h-[32em] relative rounded-bl-3xl rounded-br-3xl border-[0.55px] border-gray-200 overflow-hidden bg-white ${className} ${hasShadow}`}
+    <motion.article
+      initial='rest'
+      whileHover='hover'
+      variants={hasShadow}
+      className={`w-[20.9375em] h-[32em] relative rounded-bl-3xl rounded-br-3xl border-[0.55px] border-gray-200 overflow-hidden bg-white ${className}`}
     >
-      <span className='absolute block w-full h-[4px] bg-gradient-to-r from-cerulean-crayola to-standard-blue' />
+      <motion.span className='absolute block w-full h-[4px] bg-gradient-to-r from-cerulean-crayola to-standard-blue' />
       {children}
-    </article>
+    </motion.article>
   );
 }
