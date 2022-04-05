a49b22933e308e248ab753a85cd6177e2616f3aa
JOB LIPAT <lipatjj@live.mcl.edu.ph>
2022-04-05 22:17:19 +0800

    fixed card background shadow not animating exit

--- src/components/events/EventCardBackground.jsx
+++ src/components/events/EventCardBackground.jsx
@@ -3,26 +3,30 @@ import { motion } from 'framer-motion';
 
 const variants = {
   rest: {
-    boxShadow: '8px 8px 0 2px hsla(199, 77%, 94%)',
+    bottom: -15,
+    right: -15,
   },
   hover: {
-    boxShadow: '15px 15px 0 2px hsla(199, 77%, 96%)',
-    originX: 25,
+    bottom: -25,
+    right: -25,
   },
-  lineHover: {},
 };
 
 export default function EventCardBackground({ shadow, children, className }) {
   const hasShadow = shadow ? variants : {};
   return (
-    <motion.article
+    <motion.div
       initial='rest'
       whileHover='hover'
-      variants={hasShadow}
-      className={`w-[20.9375em] h-[32em] relative rounded-bl-3xl rounded-br-3xl border-[0.55px] border-gray-200 overflow-hidden bg-white ${className}`}
+      className='relative w-[20.9375em] h-[32em] border-gray-200 bg-white rounded-bl-3xl rounded-br-3xl border-[0.55px]  '
     >
+      <motion.article
+        variants={hasShadow}
+        className={`absolute w-[20.9375em] h-[32em] ${className} rounded-bl-3xl rounded-br-3xl -z-10`}
+        style={{ background: 'hsla(199, 77%, 94%)' }}
+      />
       <motion.span className='absolute block w-full h-[4px] bg-gradient-to-r from-cerulean-crayola to-standard-blue' />
       {children}
-    </motion.article>
+    </motion.div>
   );
 }
