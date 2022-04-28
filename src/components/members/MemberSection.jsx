8f3598f22da2275811560837b10be294bba2985d
JOB LIPAT <lipatjj@live.mcl.edu.ph>
2022-04-28 14:18:20 +0800

    made components in members section smaller

--- src/components/members/MemberSection.jsx
+++ src/components/members/MemberSection.jsx
@@ -4,7 +4,7 @@ export default function MemberSection({ header, children }) {
   return (
     <div>
       <hr className='h-1 shadow-lg lg:hidden shadow-blue-500/90 bg-gradient-to-tr from-standard-blue to-cerulean-crayola' />
-      <div className='mt-4 mb-8 lg:mb-16'>
+      <div className='mt-4 mb-8 lg:mb-12'>
         <h3 className='text-2xl font-bold text-center lg:mb-4 text-oxford-blue lg:text-4xl lg:text-left'>
           {header}
         </h3>
