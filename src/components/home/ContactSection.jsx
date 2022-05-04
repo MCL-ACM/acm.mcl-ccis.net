82c1e88c8f29f9051ff27153e3c90805f7134e88
JOB LIPAT <lipatjj@live.mcl.edu.ph>
2022-05-04 10:23:13 +0800

    reduced font of Reach out to us corrected featured event index

--- src/components/home/ContactSection.jsx
+++ src/components/home/ContactSection.jsx
@@ -8,8 +8,8 @@ export default function ContactSection() {
   return (
     <div className='px-12 my-8 fixed-width lg:mb-24'>
       <Divider className='hidden lg:block' />
-      <section className='flex flex-col items-center grid-cols-2 gap-12 px-5 my-16 text-center lg:text-left lg:items-start lg:grid'>
-        <h1 className='text-5xl font-bold text-oxford-blue lg:text-6xl'>
+      <section className='flex flex-col items-center grid-cols-2 gap-8 px-2 my-16 text-center lg:text-left lg:items-start lg:grid'>
+        <h1 className='text-4xl font-bold text-oxford-blue lg:text-5xl'>
           Reach out to us.
         </h1>
         <p className='text-2xl font-light text-oxford-blue lg:order-3'>
