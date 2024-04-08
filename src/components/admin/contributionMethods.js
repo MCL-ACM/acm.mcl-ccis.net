80cd7c99a5f14a6f7e03514f0cf85b8e8d0d9585
Namaedaa <65771463+Namaedaa@users.noreply.github.com>
2024-04-08 21:19:43 +0800

    updated Contribution Methods

--- src/components/admin/contributionMethods.js
+++ src/components/admin/contributionMethods.js
@@ -1,9 +1,15 @@
 import { API_ADD_CONTRIBUTIONS } from './api';
 
-export async function setContributionData() {
+export async function addContributionData(memberID, contributionDT) {
   try {
-    const response = await fetch(API_ADD_CONTRIBUTIONS);
-    const data = await response.json();
+    const response = await fetch(API_ADD_CONTRIBUTIONS, {
+      method: 'POST',
+      body: JSON.stringify({
+        memberID,
+        contributionDT,
+      }),
+    });
+    const data = await response.text();
     return data;
   } catch (error) {
     console.error('Error fetching data:', error);
--- src/pages/adminManager/contributions.js
+++ src/pages/adminManager/contributions.js
@@ -1,11 +1,20 @@
 import React, { useState, useEffect } from 'react';
 import Head from '../../components/common/Head';
-import { setContributionData } from '../../components/admin/contributionMethods';
+import { addContributionData } from '../../components/admin/contributionMethods';
 
 export default function ContributionsPage() {
   const [data, setData] = useState([]);
 
-  setContributionData();
+  useEffect(() => {
+    const fetchData = async () => {
+      const tempData = await addContributionData(1000, new Date(2025, 4, 14));
+      setData(tempData);
+    };
+
+    fetchData();
+  }, []);
+
+  console.log(data);
 
   return (
     <div className='w-full lg:mt-[4.8125em] relative pb-[9.25em]'>
